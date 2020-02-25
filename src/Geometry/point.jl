f930a8b24d811083aa11f080a05ecb2163d3e4da
Luiz M. Faria <maltezfaria@gmail.com>
2020-02-25 16:25:48 +0100

    add native Point type, remove dependency on GeometryTypes

--- src/Geometry/point.jl
+++ src/Geometry/point.jl
@@ -1,8 +1,12 @@
-using GeometryTypes: Point #TODO: define this internally
+struct Point{N,T}
+    coords::NTuple{N,T}
+end
+Base.size(pt::Point{N,T}) where {N,T} = (N,)
 
-# struct Point{N,T} <: AbstractVector{T}
-#     coords::NTuple{N,T}
-# end
-# Base.size(pt::Point{N,T}) where {N,T} = (N,)
-# Base.getindex(pt::Point,i) = getindex(pt.coords,i)
+Point(args...) = Point(promote(args...))
 
+for op in (:+, :-, :min, :max)
+    @eval Base.$op(pt1::T,pt2::T) where {T<:Point} = T($op.(pt1.coords,pt2.coords))
+end
+
+Base.getindex(pt::Point,i) = getindex(pt.coords,i)
