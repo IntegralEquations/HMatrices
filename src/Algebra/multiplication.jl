3a90e2b0871537af38dc204d87a1e0a19e48a917
Luiz M. Faria <maltezfaria@gmail.com>
2020-03-19 14:10:30 +0100

    add multithreaded hmat vector product

--- src/Algebra/multiplication.jl
+++ src/Algebra/multiplication.jl
@@ -1,20 +1,95 @@
-function LinearAlgebra.mul!(C::AbstractVector,H::HMatrix,F::AbstractVector,a::Number,b::Number)
-    LinearAlgebra.rmul!(C,b)
-    shift = _idx_pivot(H) .- 1
-    for block in AbstractTrees.Leaves(H)
-        irange = rowrange(block) .- shift[1]
-        jrange = colrange(block) .- shift[2]
-        data = getdata(block)
-        LinearAlgebra.mul!(view(C,irange,:),data,view(F,jrange,:),a,1)
+hmul!(args...;kwargs...)                      = hmul!(CPU1(),args...;kwargs...)
+hmul!(resource::CPU1,args...;kwargs...)       = _cpu1_hmul!(args...,kwargs...)
+hmul!(resource::CPUThreads,args...;kwargs...) = _cputhreads_hmul!(args...,kwargs...)
+
+LinearAlgebra.mul!(C::AbstractVector,H::AbstractHierarchicalMatrix,F::AbstractVector,a::Number,b::Number) = hmul!(C,H,F,a,b)
+LinearAlgebra.mul!(r::AbstractResource,args...;kwargs...) = hmul!(r,args...;kwargs...)
+
+function _cpu1_hmul!(C::AbstractVector,H::HMatrix,F::AbstractVector,a::Number,b::Number)
+    b==1 || rmul!(C,b)
+    if isleaf(H)
+        data = getdata(H)
+        mul!(C,data,F,a,1)
+    else
+        children = getchildren(H)
+        m,n      = size(children)
+        shift    = _idx_pivot(H) .- 1
+        for i=1:m
+            for j=1:n
+                block  = children[i,j]
+                irange = rowrange(block) .- shift[1]
+                Cview  = view(C,irange)
+                jrange = colrange(block) .- shift[2]
+                Fview  = view(F,jrange)
+                _cpu1_hmul!(Cview,block,Fview,a,1)
+            end
+        end
     end
     return C
 end
 
-function LinearAlgebra.mul!(C::AbstractVector,Rk::RkFlexMatrix,F::AbstractVector,a::Number,b::Number)
-    LinearAlgebra.mul!(C,Rk.A,Rk.Bt*F,a,b)
+function _cputhreads_hmul!(C::AbstractVector,H::HMatrix,F::AbstractVector,a::Number,b::Number)
+    b==1 || rmul!(C,b)
+    if isleaf(H)
+        data = getdata(H)
+        mul!(C,data,F,a,1)
+    else
+        children = getchildren(H)
+        m,n      = size(children)
+        shift    = _idx_pivot(H) .- 1
+        t        = Matrix{Task}(undef,m,n)
+        for i=1:m
+            # TODO: choose between a ReentrantLock or a Semaphore here
+            mutex = ReentrantLock()
+            # mutex = Base.Semaphore(1)
+            for j=1:n
+                block  = children[i,j]
+                irange = rowrange(block) .- shift[1]
+                Cview  = view(C,irange)
+                jrange = colrange(block) .- shift[2]
+                Fview  = view(F,jrange)
+                t[i,j] = @spawn begin
+                    lock(mutex)
+                    # Base.acquire(mutex)
+                    _cputhreads_hmul!(Cview,block,Fview,a,1)
+                    # Base.release(mutex)
+                    unlock(mutex)
+                end
+            end
+        end
+        waitall(t)
+    end
+    return C
 end
 
-function LinearAlgebra.mul!(C::Vector,Rk::RkMatrix,F::Vector,a,b)
-    @info "here"
-    LinearAlgebra.mul!(C,Rk.A,Rk.Bt*F,a,b)
+waitall(t) = [wait(ti) for ti in t]
+
+function LinearAlgebra.mul!(C::AbstractVector,Rk::RkFlexMatrix,F::AbstractVector,a::Number,b::Number,buffer=similar(C,rank(Rk)))
+    buffer = mul!(buffer,Rk.Bt,F)
+    mul!(C,Rk.A,buffer,a,b)
 end
+
+# memory_overlap(A,R::RkFlexMatrix) = false
+# memory_overlap(R::RkFlexMatrix,A) = false
+# memory_overlap(R::RkFlexMatrix,A::RkFlexMatrix) = false
+# memory_overlap(R::FlexMatrix,A)   = false
+# memory_overlap(A,R::FlexMatrix)   = false
+# function plan_mul!(C::AbstractVector,H::HMatrix,F::AbstractVector,a::Number,b::Number,tg::TaskGraph=TaskGraph())
+#     # @addop tg LinearAlgebra.rmul!(C,b)
+#     cl = @codelet LinearAlgebra.rmul!(C,b)
+#     HScheduler.addvertex!(tg,cl)
+#     shift = _idx_pivot(H) .- 1
+#     for block in AbstractTrees.Leaves(H)
+
+#         irange = rowrange(block) .- shift[1]
+#         jrange = colrange(block) .- shift[2]
+#         data = getdata(block)
+#         Cview = view(C,irange)
+#         Fview = view(F,jrange)
+#         cl = @codelet LinearAlgebra.mul!(Cview,data,Fview,a,1)
+#         HScheduler.addvertex!(tg,cl)
+#     end
+#     cl = Codelet(func=(C)->(C),data=(C,),access_mode=(HScheduler.READ,))
+#     HScheduler.addvertex!(tg,cl)
+#     return tg
+# end
--- test/Algebra/multiplication_test.jl
+++ test/Algebra/multiplication_test.jl
@@ -2,23 +2,79 @@ using SafeTestsets
 
 @safetestset "Multiplication" begin
     using HierarchicalMatrices
-    using HierarchicalMatrices: compress, ACA, PartialACA
-    using HierarchicalMatrices.Geometry
-    using HierarchicalMatrices.Clusters
+    using HierarchicalMatrices: ACA, PartialACA, hmul!
     using LinearAlgebra
-    @testset "mul!" begin
+    @testset "CPU1 mul!" begin
         let
-            N    = 1000
-            data = rand(Point{3,Float64},N)
-            splitter   = Clusters.CardinalitySplitter(nmax=32)
-            clt = ClusterTree(data,splitter;reorder=true)
-            bclt = BlockClusterTree(clt,clt)
-            comp = HierarchicalMatrices.ACA()
+            N    = 2000
+            data = rand(Geometry.Point{2,Float64},N)
+            splitter   = Clusters.CardinalitySplitter()
+            clt  = Clusters.ClusterTree(data,splitter;reorder=true)
+            bclt = Clusters.BlockClusterTree(clt,clt)
+            comp = HierarchicalMatrices.PartialACA(rtol=1e-4)
             f(x,y)::ComplexF64 = x==y ? 0.0 : exp(im*LinearAlgebra.norm(x-y))/LinearAlgebra.norm(x-y)
-            M    = [f(x,y) for x in data, y in data]
+            M    = LazyMatrix(f,data,data)
             H    = HMatrix(M,bclt,comp)
             x    = rand(ComplexF64,N)
-            @test norm(H*x-M*x) < comp.rtol*norm(M)
+            y    = similar(x)
+            # 3-arg mul
+            mul!(y,H,x)
+            @test norm(y-M*x) < 10*comp.rtol*norm(M)
+            a = 1; b=2;
+            tmp = b*y + a*M*x
+            # 5-arg mul
+            hmul!(y,H,x,a,b)
+            @test norm(y-tmp) < 10*comp.rtol*norm(M)
+        end
+    end
+    @testset "CPUThreads mul!" begin
+        let
+            N    = 2000
+            data = rand(Geometry.Point{2,Float64},N)
+            splitter   = Clusters.CardinalitySplitter()
+            clt  = Clusters.ClusterTree(data,splitter;reorder=true)
+            bclt = Clusters.BlockClusterTree(clt,clt)
+            comp = HierarchicalMatrices.PartialACA(rtol=1e-4)
+            f(x,y)::ComplexF64 = x==y ? 0.0 : exp(im*LinearAlgebra.norm(x-y))/LinearAlgebra.norm(x-y)
+            M    = LazyMatrix(f,data,data)
+            H    = HMatrix(M,bclt,comp)
+            x    = rand(ComplexF64,N)
+            y    = zero(x)
+            # 3-arg mul
+            # mul!(CPUThreads(),y,H,x)
+            # @test norm(y-M*x) < 10*comp.rtol*norm(M)
+            # 5-arg mul
+            a = 1; b=2;
+            tmp = b*y + a*M*x
+            hmul!(CPUThreads(),y,H,x,a,b)
+            @test norm(y-tmp) < 10*comp.rtol*norm(M)
+        end
+    end
+    @testset "plan_mul!" begin
+        let
+            # using HScheduler
+            # N    = 4000
+            # data = rand(Point{2,Float64},N)
+            # splitter   = Clusters.CardinalitySplitter(nmax=128)
+            # clt = ClusterTree(data,splitter;reorder=true)
+            # bclt = BlockClusterTree(clt,clt)
+            # comp = HierarchicalMatrices.PartialACA(rtol=1e-6)
+            # f(x,y)::ComplexF64 = x==y ? 0.0 : exp(im*LinearAlgebra.norm(x-y))/LinearAlgebra.norm(x-y)
+            # M    = LazyMatrix(f,data,data)
+            # H    = HMatrix(M,bclt,comp)
+            # x    = rand(ComplexF64,N)
+            # y  = zero(x)
+            # tg = plan_mul!(y,H,x,1,0)
+            # t1 = @elapsed HScheduler.execute.(tg.vertices)
+            
+            # # @test norm(y-M*x) < comp.rtol*norm(M)
+            # tg = plan_mul!(y,H,x,1,0)
+            # fill!(y,0)
+            # # t2 = @elapsed HScheduler.run_parallel(tg)
+            # t3 = @elapsed mul!(y,H,x,1,0,Val(false))
+            # t4 = @elapsed mul!(y,H,x,1,0,Val(true))
+            # @show t1, t3, t4
+            # @test norm(y-M*x) < comp.rtol*norm(M)
         end
     end
 end
